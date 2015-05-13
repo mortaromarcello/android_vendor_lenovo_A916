e75394ca9cb2c2c95fed0d52fc3456d79b891e1f
fire855 <thefire855@gmail.com>
2015-05-13 19:37:08 +0200

    rename to k1-turbo

---
+++ k1-turbo-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1-turbo/vendor,system/vendor)
