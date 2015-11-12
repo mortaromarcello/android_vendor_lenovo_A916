a700ff457ffb30480b89b734c2d4f64e7908a651
Marcello Mortaro <mortaromarcello@gmail.com>
2015-11-12 09:52:17 +0100

    Update and rename k1_turbo-vendor.mk to A916-vendor.mk

---
+++ A916-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/A916/vendor,system/vendor)
